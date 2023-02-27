0bb3559fa1e705574b6534e50201880b9a91a027
yuval taubes <yuval.taubes@gmail.com>
2023-02-27 09:15:18 -0600

    Add project files.

---
+++ Data/StackOverflowContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StackOverflow.Models;
+
+namespace StackOverflow.Data
+{
+    public class StackOverflowContext : DbContext
+    {
+        public StackOverflowContext (DbContextOptions<StackOverflowContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StackOverflow.Models.Question> Question { get; set; } = default!;
+    }
+}
---
+++ Models/Question.cs
@@ -0,0 +1,14 @@
+﻿namespace StackOverflow.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime PostTime { get; set; }
+
+        List<string>? Tags { get; set; }
+    }
+}
